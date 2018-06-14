182e20ff8ced798ad70b3b6b6d711d44d668f0ed
mnel <michael.nelson@sydney.edu.au>
2018-06-14 21:58:57 +1000

    Initial commit

---
+++ tests/tests.R
@@ -0,0 +1,15 @@
+urlR <- "http://home.versanet.de/~s-berman/source2.R"
+testfun <- function(urlm){stopifnot(length(scan(urlm,"", quiet=TRUE))==7)}
+# works 
+url_default <- url(urlR)
+testfun(url_default)
+             
+
+url_default_en <- url(urlR, encoding = "UTF-8")
+testfun(url_default_en)
+url_internal <- url(urlR, method = 'internal')
+testfun(url_internal)
+
+
+url_internal_en <- url(urlR, encoding = "UTF-8", method = 'internal')
+testfun(url_internal_en)#scan(url_internal_en, "")
