0d14dbf579d49b3e19ee5ffa0c65a3ed55b325fe
mnel <michael.nelson@sydney.edu.au>
2018-06-14 22:50:58 +1000

    just the segfault test

--- tests/tests.R
+++ tests/tests.R
@@ -5,8 +5,8 @@ url_default <- url(urlR)
 testfun(url_default)
              
 
-url_default_en <- url(urlR, encoding = "UTF-8")
-testfun(url_default_en)
+#url_default_en <- url(urlR, encoding = "UTF-8")
+#testfun(url_default_en)
 url_internal <- url(urlR, method = 'internal')
 testfun(url_internal)
 
